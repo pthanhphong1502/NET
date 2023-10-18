f081bdec9b153e6dc0a50b38555db5e50d617151
Pham Thanh Phong <ptp150202@gmail.com>
2023-10-18 14:15:22 +0700

    add UI

---
+++ ProductManagementWebClient/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ProductManagementWebClient.Models;
+
+namespace ProductManagementWebClient.Controllers
+{
+    public class ProductController : Controller
+    {
+        Uri baseAddress = new Uri("https://localhost:7001/api");
+        private readonly HttpClient _client;
+
+        public ProductController()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = baseAddress;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<ProductViewModel> products = new List<ProductViewModel>();
+            HttpResponseMessage response = _client.GetAsync(_client.BaseAddress + "/Product/GetAllProducts").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string data = response.Content.ReadAsStringAsync().Result;
+                products = JsonConvert.DeserializeObject<List<ProductViewModel>>(data);
+            }
+            return View(products);
+        }
+    }
+}
---
+++ ProductManagementWebClient/Models/ProductViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace ProductManagementWebClient.Models
+{
+    public class ProductViewModel
+    {
+        public int ProductId { get; set; }
+        [DisplayName("Product Name")]
+        public string? ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
